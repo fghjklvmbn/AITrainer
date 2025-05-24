f027205c5928146c36d2f197c77f47612f1401b5
parkgyeonghwan <dsapoi881@gmail.com>
2025-05-24 20:28:13 +0900

    Update app_stub.py

--- server/app_stub.py
+++ server/app_stub.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, jsonify
-from stub.WriteDetail_stub import WriteDetail
-from stub.FullCreate_stub import FullCreate
-from stub.CharaterSpecific_stub import CharaterSpecific
+from stub.WriteDetail_stub import WriteDetail as write_detail_stub
+from stub.FullCreate_stub import FullCreate as full_create_stub
+from stub.CharaterSpecific_stub import CharaterSpecific as character_specific_stub
 
 app = Flask(__name__)
 
-# api url 설정
 
-@app.route("/ai/StoryCreate/WriteDetail", methods=["POST"])
+@app.route("/AI/StoryCreate/", methods=["GET"])
+def index():
+    return jsonify({"message": "정상작동"})
+
+# 1. 전체 이야기 생성
 @app.route("/ai/StoryCreate/FullCreate", methods=["POST"])
-@app.route("/ai/StoryCreate/CharaterSpecific", methods=["POST"])
-
-# 전체적인 이야기 생성
-def FullCreate():
+def full_create():
     data = request.get_json()
     prompt = data.get("prompt", "")
     custom_tag = data.get("custom_tag", "")
@@ -22,14 +22,15 @@ def FullCreate():
     if not custom_tag:
         return jsonify({"오류": "커스텀 태그(custom_tag) 항목은 필수 입니다."}), 400
 
-    result = FullCreate(prompt, custom_tag)
+    result = full_create_stub(prompt, custom_tag)
     return jsonify(result)
 
 
-def WriteDetail():
+# 2. 상세 페이지 이야기 생성
+@app.route("/ai/StoryCreate/WriteDetail", methods=["POST"])
+def write_detail():
     data = request.get_json()
 
-    # 프롬프트와 스토리 진행을 가져온다.
     createpage = data.get("page", "")
     story_progression = data.get("story_progression", "")
     
@@ -38,23 +39,23 @@ def WriteDetail():
     if not story_progression:
         return jsonify({"오류": "스토리 진행(story_progression) 항목은 필수 입니다."}), 400
     
-    # write_story 함수를 호출하여 이야기를 생성한다.
-    result = WriteDetail(createpage, story_progression)
+    result = write_detail_stub(createpage, story_progression)
     return jsonify(result)
 
 
-# 등장인물 자세한 정보 생성
-def CharaterSpecific():
+# 3. 캐릭터 세부 생성
+@app.route("/ai/StoryCreate/CharaterSpecific", methods=["POST"])
+def character_specific():
     data = request.get_json()
     character_name = data.get("character_name", "")
     plot = data.get("plot", "")
     
     if not character_name:
-        return jsonify({"오류": "캐릭터(character) 항목은 필수 입니다."}), 400
+        return jsonify({"오류": "캐릭터(character_name) 항목은 필수 입니다."}), 400
     if not plot:
         return jsonify({"오류": "줄거리(plot) 항목은 필수 입니다."}), 400
 
-    result = CharaterSpecific(character_name, plot)
+    result = character_specific_stub(character_name, plot)
     return jsonify(result)
 
 
