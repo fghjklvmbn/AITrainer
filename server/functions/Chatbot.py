eaf1f7b189b424152b17d03085589aca892674d8
박경환 <66707337+fghjklvmbn@users.noreply.github.com>
2025-07-01 01:34:13 +0900

    Update Chatbot.py

--- server/functions/Chatbot.py
+++ server/functions/Chatbot.py
@@ -8,12 +8,12 @@ model = None
 def load_model():
     global model, tokenizer
     if model is None or tokenizer is None:
-        model_path = "Qwen/Qwen3-1.7B"
+        model_path = "kakaocorp/kanana-1.5-2.1b-base"
         tokenizer = AutoTokenizer.from_pretrained(model_path)
         model = AutoModelForCausalLM.from_pretrained(
             model_path,
             torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
-            device_map="auto"
+            device_map="cuda"
         )
         device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
         model.to(device)
@@ -31,15 +31,14 @@ def chat(user_input):
         messages,
         tokenize=False,
         add_generation_prompt=True,
-        enable_thinking=True
+        # enable_thinking=True
     )
 
     inputs = tokenizer([prompt], return_tensors="pt").to(model.device)
 
     outputs = model.generate(
         **inputs,
-        temperature=0.5,
-        max_new_tokens=2048
+        temperature=0.5
     )
 
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
